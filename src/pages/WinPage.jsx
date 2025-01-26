593e0acc75660dd7a12de01a3de804a45342bf41
Pablo Abratte <pabratte@gmail.com>
2025-01-26 18:57:37 -0300

    improve win page style

--- src/pages/WinPage.jsx
+++ src/pages/WinPage.jsx
@@ -1,8 +1,8 @@
 export default function WinPage({ triesCount, resetGame }) {
   return (
-    <div>
+    <div className={"win-page"}>
       <h2>Congratulations!</h2>
-      <h4>You finished the game in</h4>
+      <h4>You won the game in</h4>
       <h3>{triesCount}</h3>
       <h4>tries</h4>
       <button onClick={resetGame}>Play Again</button>
